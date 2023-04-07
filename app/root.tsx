94736f3ff50061f57830edb5188009a9ebfb770a
Bogdan Covrig <bogcov@gmail.com>
2023-04-07 14:31:06 +0200

    feature: Inital commit

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import styles from "./styles/tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Cosmocut – Send your short links into space",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body className="h-screen bg-foreground overflow-hidden">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
